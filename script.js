f3b016b09025251e5b40476b8b92e86b8979f5ea
Sangeethasan7 <120364555+Sangeethasan7@users.noreply.github.com>
2023-09-08 11:49:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,49 @@
-//your code here
+// Array of band names
+let bandNames = ['The Rolling Stones', 'Led Zeppelin', 'The Beatles', 'Pink Floyd', 'Radiohead', 'The Who'];
+
+// Function to remove articles and sort the names
+function sortBandNamesWithoutArticles(names) {
+  const articles = ['a', 'an', 'the'];
+
+  // Custom sorting function
+  function customSort(a, b) {
+    const nameA = a.toLowerCase();
+    const nameB = b.toLowerCase();
+
+    // Remove articles from the beginning of each name
+    for (const article of articles) {
+      if (nameA.startsWith(article)) {
+        nameA = nameA.slice(article.length).trim();
+        break;
+      }
+    }
+
+    for (const article of articles) {
+      if (nameB.startsWith(article)) {
+        nameB = nameB.slice(article.length).trim();
+        break;
+      }
+    }
+
+    return nameA.localeCompare(nameB);
+  }
+
+  // Sort the names using the custom sorting function
+  names.sort(customSort);
+
+  return names;
+}
+
+// Sort the band names without articles
+const sortedBandNames = sortBandNamesWithoutArticles(bandNames);
+
+// Create an HTML list
+const ul = document.getElementById('band');
+
+for (const name of sortedBandNames) {
+  const li = document.createElement('li');
+  li.textContent = name;
+  ul.appendChild(li);
+}
+
 
